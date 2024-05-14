6b7c0514aea1ccc702c1d84068a708f850ba5fcb
Birame MBOUP <bmboup@peopleinput.com>
2024-05-14 13:41:20 +0200

    TP of qualite de lair . Please Check the README file

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Prédiction de la qualité de l'air de la gare de Auber de la ligne A du RER',
+    author='Birame MBOUP',
+    license='MIT',
+)
